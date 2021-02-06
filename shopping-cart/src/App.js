e2197aae6057b3078aaa29a524c2f96e66aeb58c
Subhamoy Ghosh <itssubhamoy94@outlook.com>
2021-02-06 12:09:01 +0530

    added signin functionality using firebase

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -4,17 +4,39 @@ import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.components';
 import './App.css';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.components'
+import {auth} from './firebase/firebase.utility';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shops' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    }
+  }
+  unSubscribeFromAuth = null;
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user=>{
+      this.setState({currentUser: user });
+    })
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shops' component={ShopPage} />
+          <Route exact path='/signIn' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ shopping-cart/src/firebase/firebase.utility.js
@@ -0,0 +1,48 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: "AIzaSyBrnly9YVv_I4uZpe7C3NQFD5uaQoTfMs4",
+    authDomain: "userauth-898cb.firebaseapp.com",
+    projectId: "userauth-898cb",
+    storageBucket: "userauth-898cb.appspot.com",
+    messagingSenderId: "753276468725",
+    appId: "1:753276468725:web:d69c770e9f3716681034bc",
+    measurementId: "G-5ZWYR52QDK"
+  };
+
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
